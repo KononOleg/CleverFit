521b6514f0a5ef213b1f963d8466af923f606b79
KononOleg <78895796+KononOleg@users.noreply.github.com>
2024-03-24 14:42:01 +0300

    feat: add tariff feedbacks

--- src/pages/settings-page/settings-page.tsx
+++ src/pages/settings-page/settings-page.tsx
@@ -1,22 +1,43 @@
-import { Card } from 'antd';
+import { Button, Card } from 'antd';
 import styles from './settings-page.module.scss';
 import { TariffCards } from './components/tariff-cards';
 import { TariffOptions } from './components/tariff-options';
 import { DrawerTariff } from './components/comparing-drawer';
 import { useState } from 'react';
+import { ModalFeedback } from '@pages/feedbacks-page/components/modal-feedback';
+import { Link } from 'react-router-dom';
+import { PATH } from '@constants/index';
+import { useGetTrainingListQuery } from '@redux/services/training-service';
 
 export const SettingsPage = () => {
+    const { refetch } = useGetTrainingListQuery();
     const [openTariffSider, setOpenTariffSider] = useState(false);
+    const [openNewFeedback, setOpenNewFeedback] = useState(false);
 
     const closeTariffSiderHandler = () => setOpenTariffSider(false);
     const openTariffSiderHandler = () => setOpenTariffSider(true);
+    const openNewFeedbackHandler = () => setOpenNewFeedback(true);
+    const handleRefetch = () => refetch();
 
     return (
         <>
             <Card className={styles.SettingsPage}>
                 <TariffCards handleOpen={openTariffSiderHandler} />
                 <TariffOptions />
+                <div className={styles.Buttons}>
+                    <Button type='primary' onClick={openNewFeedbackHandler}>
+                        Написать отзыв
+                    </Button>
+                    <Button type='link'>
+                        <Link to={PATH.FEEDBACKS}>Смотреть все отзывы</Link>
+                    </Button>
+                </div>
             </Card>
+            <ModalFeedback
+                open={openNewFeedback}
+                setOpen={setOpenNewFeedback}
+                handleRefetch={handleRefetch}
+            />
             <DrawerTariff open={openTariffSider} handleClose={closeTariffSiderHandler} />
         </>
     );
